f83fb1fbf4d6aea92a39221b4fca1276512f11ab
Jacek Pietras <jaxilus@gmail.com>
2016-04-01 11:30:25 +0200

    Praca nad testami z mockito

--- messenger 2/src/test/java/com/example/mockdemo/app/MockitoAppTest.java
+++ messenger 2/src/test/java/com/example/mockdemo/app/MockitoAppTest.java
@@ -1,18 +1,5 @@
 package com.example.mockdemo.app;
 
-
-import static org.easymock.EasyMock.capture;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
-import static org.easymock.EasyMock.verify;
-import static org.hamcrest.CoreMatchers.either;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.*;
-
-import org.easymock.Capture;
-import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,81 +8,96 @@ import com.example.mockdemo.messenger.MalformedRecipientException;
 import com.example.mockdemo.messenger.MessageService;
 import com.example.mockdemo.messenger.SendingStatus;
 
+
+import static org.hamcrest.CoreMatchers.either;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+//import static org.easymock.EasyMock.capture;
+//import static org.easymock.EasyMock.createMock;
+//import static org.easymock.EasyMock.expect;
+//import static org.easymock.EasyMock.replay;
+//import static org.easymock.EasyMock.verify;
+//import org.easymock.Capture;
+//import org.easymock.EasyMock;
+
 public class MockitoAppTest {
-	
-	private final String VALID_SERVER = "inf.ug.edu.pl";
-	private final String INVALID_SERVER = "inf.ug.edu.eu";
 
-	private final String VALID_MESSAGE = "some message";
-	private final String INVALID_MESSAGE = "ab";
-	
-	private Messenger messenger;
-	private MessageService msMock;
+    private static final String VALID_SERVER = "inf.ug.edu.pl";
+    private static final String INVALID_SERVER = "inf.ug.edu.eu";
 
-	@Before
-	public void setUP() throws MalformedRecipientException {
-		msMock = mock(MessageService.class);
-		messenger = new Messenger(msMock);
-	}
-	
-	@Test
-	public void sendingValidRecipientAndServer()
-			throws MalformedRecipientException {
+    private static final String VALID_MESSAGE = "some message";
+    private static final String INVALID_MESSAGE = "ab";
 
-		when(msMock.send(VALID_SERVER, VALID_MESSAGE)).thenReturn(
-				SendingStatus.SENT).atLeastOnce();
-		when(msMock.checkConnection(VALID_SERVER)).thenReturn(
-				ConnectionStatus.SUCCESS);
-		replay(msMock);
+    private Messenger messenger;
+    private MessageService msMock;
 
-		assertThat(messenger.testConnection(VALID_SERVER), equalTo(0));
-		assertThat(messenger.sendMessage(VALID_SERVER, VALID_MESSAGE),
-				either(equalTo(0)).or(equalTo(1)));
+    @Before
+    public void setUP() throws MalformedRecipientException {
+        msMock = mock(MessageService.class);
+        messenger = new Messenger(msMock);
+    }
 
-		verify(msMock);
-	}
+    @Test
+    public void sendingValidRecipientAndServer()
+            throws MalformedRecipientException {
 
-	@Test
-	public void sendingInvalidServer() throws MalformedRecipientException {
+        when(msMock.send(VALID_SERVER, VALID_MESSAGE)).thenReturn(
+                SendingStatus.SENT);
+        when(msMock.checkConnection(VALID_SERVER)).thenReturn(
+                ConnectionStatus.SUCCESS);
 
-		when(msMock.checkConnection(INVALID_SERVER)).thenReturn(
-				ConnectionStatus.FAILURE);
-		when(msMock.send(INVALID_SERVER, VALID_MESSAGE)).thenReturn(
-				SendingStatus.SENDING_ERROR);
-		replay(msMock);
+        assertThat(messenger.testConnection(VALID_SERVER), equalTo(0));
+        assertThat(messenger.sendMessage(VALID_SERVER, VALID_MESSAGE),
+                either(equalTo(0)).or(equalTo(1)));
 
-		assertThat(messenger.testConnection(INVALID_SERVER), equalTo(1));
-		assertEquals(1, messenger.sendMessage(INVALID_SERVER, VALID_MESSAGE));
+        verify(msMock);
+    }
 
-		verify(msMock);
-	}
-
-	@Test
-	public void sendingInvalidReceipient() throws MalformedRecipientException {
-
-		when(msMock.send(VALID_SERVER, INVALID_MESSAGE)).thenThrow(
-				new MalformedRecipientException()).atLeastOnce();
-
-		replay(msMock);
-
-		assertEquals(2, messenger.sendMessage(VALID_SERVER, INVALID_MESSAGE));
-		verify(msMock);
-	}
-
-	// Przechwytywanie parametrow
-	@Test
-	public void sendingConnectionStatus() {
-
-		Capture<String> capturedServer = EasyMock.newCapture();
-
-		when(msMock.checkConnection(capture(capturedServer))).thenReturn(
-				ConnectionStatus.FAILURE);
-		replay(msMock);
-
-		assertEquals(1, messenger.testConnection(INVALID_SERVER));
-		assertEquals(INVALID_SERVER, capturedServer.getValue());
-
-		verify(msMock);
-	}
-
+//	@Test
+//	public void sendingInvalidServer() throws MalformedRecipientException {
+//
+//		when(msMock.checkConnection(INVALID_SERVER)).thenReturn(
+//				ConnectionStatus.FAILURE);
+//		when(msMock.send(INVALID_SERVER, VALID_MESSAGE)).thenReturn(
+//				SendingStatus.SENDING_ERROR);
+//		
+//
+//		assertThat(messenger.testConnection(INVALID_SERVER), equalTo(1));
+//		assertEquals(1, messenger.sendMessage(INVALID_SERVER, VALID_MESSAGE));
+//
+//		verify(msMock);
+//	}
+//
+//	@Test
+//	public void sendingInvalidReceipient() throws MalformedRecipientException {
+//
+////		doThrow(new MalformedRecipientException()).when(msMock.send(VALID_SERVER, INVALID_MESSAGE));
+//		
+//		when(msMock.send(VALID_SERVER, INVALID_MESSAGE)).thenThrow(
+//				new MalformedRecipientException());
+//
+//		
+//
+//		assertEquals(2, messenger.sendMessage(VALID_SERVER, INVALID_MESSAGE));
+//		verify(msMock);
+//	}
+//
+//	// Przechwytywanie parametrow
+//	@Test
+//	public void sendingConnectionStatus() {
+//
+//		Capture<String> capturedServer = EasyMock.newCapture();
+//
+//		when(msMock.checkConnection(capture(capturedServer))).thenReturn(
+//				ConnectionStatus.FAILURE);
+//		
+//
+//		assertEquals(1, messenger.testConnection(INVALID_SERVER));
+//		assertEquals(INVALID_SERVER, capturedServer.getValue());
+//
+//		verify(msMock);
+//	}
 }
