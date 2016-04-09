a139d9f2f015f98bea7386e673d6f71f3392c76f
Jacek Pietras <jaxilus@gmail.com>
2016-04-10 00:37:35 +0200

    drobne zmiany

--- jbehaveMessenger/src/test/java/com/example/jbehave/MessageAppTest.java
+++ jbehaveMessenger/src/test/java/com/example/jbehave/MessageAppTest.java
@@ -19,43 +19,42 @@ import com.example.jbehave.messenger.source.SendingStatus;
 import jbehave.messenger.Messenger;
 
 public class MessageAppTest {
-	
-	Messenger messenger = new Messenger(new MessageServiceSimpleImpl());
 
-	private final String VALID_SERVER = "inf.ug.edu.pl";
-	private final String INVALID_SERVER = "inf.ug.edu.eu";
+    Messenger messenger = new Messenger(new MessageServiceSimpleImpl());
 
-	private final String VALID_MESSAGE = "some message";
-	private final String INVALID_MESSAGE = "ab";
-	
-	@Given("a message")
-	public void messageSetup() {
-		messenger = new Messenger(new MessageServiceSimpleImpl());
-	}
-	
-	@When("set connection server to $server")
-	public void setConnectionServer(String server) {
-		messenger.testConnection(server);
-	}
-	
-	@Then("valid should return $valid_server")
-	public void shouldValid(int valid_server) {
-		assertEquals(valid_server, messenger.testConnection(VALID_SERVER));
-	}
-	
-	@Then("invalid should return $invalid_server")
-	public void shouldInvalid(int invalid_server) {
-		assertEquals(invalid_server, messenger.testConnection(INVALID_SERVER));
-	}
-	
+    private final String VALID_SERVER = "inf.ug.edu.pl";
+    private final String INVALID_SERVER = "inf.ug.edu.eu";
 
-	@Test
-	public void checkSendingMessage() {
+    private final String VALID_MESSAGE = "some message";
+    private final String INVALID_MESSAGE = "ab";
 
-		assertEquals(1, messenger.sendMessage(INVALID_SERVER, VALID_MESSAGE));
-		assertEquals(2, messenger.sendMessage(VALID_SERVER, INVALID_MESSAGE));
+    @Given("a message")
+    public void messageSetup() {
+        messenger = new Messenger(new MessageServiceSimpleImpl());
+    }
 
-		assertThat(messenger.sendMessage(VALID_SERVER, VALID_MESSAGE),
-				either(equalTo(0)).or(equalTo(1)));
-	}
+    @When("set connection server to $server")
+    public void setConnectionServer(String server) {
+        messenger.testConnection(server);
+    }
+
+    @Then("valid should return $valid_server")
+    public void shouldValid(int valid_server) {
+        assertEquals(valid_server, messenger.testConnection(VALID_SERVER));
+    }
+
+    @Then("invalid should return $invalid_server")
+    public void shouldInvalid(int invalid_server) {
+        assertEquals(invalid_server, messenger.testConnection(INVALID_SERVER));
+    }
+
+    @Test
+    public void checkSendingMessage() {
+
+        assertEquals(1, messenger.sendMessage(INVALID_SERVER, VALID_MESSAGE));
+        assertEquals(2, messenger.sendMessage(VALID_SERVER, INVALID_MESSAGE));
+
+        assertThat(messenger.sendMessage(VALID_SERVER, VALID_MESSAGE),
+                either(equalTo(0)).or(equalTo(1)));
+    }
 }
