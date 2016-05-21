2c8ef8b0e1f61a69fb16ac79fc5727d0631cdd16
Jacek Pietras <jaxilus@gmail.com>
2016-05-21 14:55:28 +0200

    dodanie plików .bat

---
+++ restservicedemo/scripts/runHSQLDBClient.bat
@@ -0,0 +1,1 @@
+java -cp C:\Users\Dom\Desktop\testowanieJava\restservicedemo\scripts\hsqldb-2.3.3.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/workdb
---
+++ restservicedemo/scripts/runHSQLDBServer.bat
@@ -0,0 +1,2 @@
+@echo off
+java -cp C:\Users\Dom\Desktop\testowanieJava\restservicedemo\scripts\hsqldb-2.3.3.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
